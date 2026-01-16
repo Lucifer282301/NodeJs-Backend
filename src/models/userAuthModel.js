7dae200fd7271d93e3742fc785d5e7b29bd9a227
Avinash Kumar <avinash.kumar@boostb2b.com>
2026-01-16 20:00:48 +0530

    [NODE-011] Implemented book routes and controller setup

--- src/models/userAuthModel.js
+++ src/models/userAuthModel.js
@@ -4,22 +4,21 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "Username is required"],
+      required: true,
       trim: true,
       maxlength: [50, "Username should not exceed 50 characters"],
       unique: true,
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+      required: true,
       trim: true,
       unique: true,
       lowercase: true,
-      match: [/.+@.+\..+/, "Please enter a valid email address"],
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
+      required: true,
       minlength: [6, "Password should be at least 6 characters long"],
     },
     role: {
